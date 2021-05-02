434fc2fc47a77a5a98eef474004abbe8df4f4ea6
carlosIvanG <carlosivangc@ufps.edu.co>
2021-05-02 13:35:24 -0500

    insertando modificaciones semifinales

--- index.js
+++ index.js
@@ -13,8 +13,28 @@ const inicar = () => {
                 localStorage.setItem(carta.carta, JSON.stringify(carta))
             }
         })
-
 }
 window.onload = () => {
     inicar()
+}
+
+const login = () => {
+    let username = document.querySelector('#fname').value
+    const password = document.querySelector('#lname').value
+
+    let user = {
+        username: username,
+        password: password
+    }
+
+    localStorage.setItem('user', JSON.stringify(user))
+    console.log(`El usuario es ${user}, y la clave es ${password}`)
+    let _user = localStorage.getItem('user')
+    _user = JSON.parse(_user)
+    console.log(`El usuario es ${_user.username}, y la clave es ${_user.password}`)
+ocultar()
+}
+function ocultar() {
+    document.getElementById("cuerpo2").style.display="contents"
+    document.getElementById("cuerpo1").style.display="none"
 }
--- js/localStorage.js
+++ js/localStorage.js
@@ -1,4 +1,4 @@
-const login = () => {
+/* const login = () => {
     let username = document.querySelector('#fname').value
     const password = document.querySelector('#lname').value
 
@@ -6,10 +6,14 @@ const login = () => {
         username: username,
         password: password
     }
-    
+
     localStorage.setItem('user', JSON.stringify(user))
     console.log(`El usuario es ${user}, y la clave es ${password}`)
     let _user = localStorage.getItem('user')
-    _user =  JSON.parse(_user)
+    _user = JSON.parse(_user)
     console.log(`El usuario es ${_user.username}, y la clave es ${_user.password}`)
-}
+} */
+/* function ocultar() {
+    document.getElementById("cuerpo2").style.display="contents"
+    document.getElementById("cuerpo1").style.display="none"
+} */
